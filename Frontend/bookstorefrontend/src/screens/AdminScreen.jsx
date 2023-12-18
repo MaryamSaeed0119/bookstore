05b48dac5c93b4fe2b930f21438d6f5923b78edc
MaryamSaeed0119 <144810857+MaryamSaeed0119@users.noreply.github.com>
2023-12-18 22:56:27 +0000

    Added Quantity Updation

--- Frontend/bookstorefrontend/src/screens/AdminScreen.jsx
+++ Frontend/bookstorefrontend/src/screens/AdminScreen.jsx
@@ -1,9 +1,19 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import UpdateScreen from './UpdateScreen';
 
 
 const AdminScreen = () => {
     const [books, setBooks] = useState([]);
+
+    const [id, setId] = useState('')
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [genre, setGenre] = useState('');
+    const [publishYear, setPublishYear] = useState('');
+    const [price, setPrice] = useState('');
+    const [qty, setQty] = useState();
+
 
     useEffect(() => {
         async function getBooks() {
@@ -17,12 +27,40 @@ const AdminScreen = () => {
 
     }, []);
 
-    const handleUpdate = (bookId) => {
-        // Handle update logic here
+    const handleUpdate = (id, title, author, publicationYear, qty, price) => {
+
+        async function updateQuantity(quantity) {
+            try {
+                qty = quantity;
+                console.log(quantity)
+                const response = await axios.put("http://localhost:3000/api/books/" + id, { title, author, publicationYear, qty, price });
+                console.log(response);
+                if (response){
+                    window.location.reload();
+                }
+            } catch (error) {
+                console.error(error);
+                // Handle the error here
+            }
+        }
+        const quantity = prompt("Please enter the updated qty");
+        
+        if (quantity != null)
+        {
+            updateQuantity(quantity);
+        }
+
     };
 
-    const handleDelete = (bookId) => {
-        // Handle delete logic here
+    const handleDelete = async (bookId) => {
+        try {
+            const response = await axios.delete("http://localhost:3000/api/books/" + bookId);
+            if (response) {
+                window.location.reload();
+            }
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
@@ -37,13 +75,26 @@ const AdminScreen = () => {
                                 <p className="card-text">Publication Year: {book.publicationYear}</p>
                                 <p className="card-text">Quantity: {book.qty}</p>
                                 <p className="card-text">Price: {book.price}</p>
-                                <button className="btn btn-primary" onClick={() => handleUpdate(book.id)}>Update</button>
-                                <button className="btn btn-danger" onClick={() => handleDelete(book.id)}>Delete</button>
+                                <div className="d-flex justify-content-between">
+                                    <button className="btn btn-primary" onClick={() => handleUpdate(book._id, book.title, book.author, book.publicationYear, book.qty, book.price)}>Update</button>
+                                    <button className="btn btn-danger" onClick={() => handleDelete(book._id)}>Delete</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
+            
+            <br>
+            
+            </br>
+            <br/>
+            <br/>
+            <br/>
+
+            {/* <UpdateScreen  title={tile}>
+
+            </UpdateScreen> */}
         </div>
     );
 };
