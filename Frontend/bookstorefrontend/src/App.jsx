a1ce47a5479bdc133dc65017867bc1f9cd0f31a5
MaryamSaeed0119 <144810857+MaryamSaeed0119@users.noreply.github.com>
2023-12-18 21:57:41 +0000

    Added navigation to app.jsx

--- Frontend/bookstorefrontend/src/App.jsx
+++ Frontend/bookstorefrontend/src/App.jsx
@@ -3,13 +3,42 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AdminScreen from './screens/AdminScreen'
+import AddScreen from './screens/AddScreen'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <AdminScreen />
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+            <li>
+              <Link to="/add">Add</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/admin">
+            <AdminScreen />
+          </Route>
+          <Route path="/add">
+            <AddScreen />
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
