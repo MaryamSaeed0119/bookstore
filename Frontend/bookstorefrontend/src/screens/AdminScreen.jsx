53b79050e753a1bfd319ea35e8b0b5cf1850c4ba
MaryamSaeed0119 <144810857+MaryamSaeed0119@users.noreply.github.com>
2023-12-18 21:24:26 +0000

    Updated UI to add the modify and delete Button

--- Frontend/bookstorefrontend/src/screens/AdminScreen.jsx
+++ Frontend/bookstorefrontend/src/screens/AdminScreen.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
+
 const AdminScreen = () => {
     const [books, setBooks] = useState([]);
 
@@ -14,23 +15,37 @@ const AdminScreen = () => {
         }
         getBooks();
 
-    }, [])
+    }, []);
+
+    const handleUpdate = (bookId) => {
+        // Handle update logic here
+    };
+
+    const handleDelete = (bookId) => {
+        // Handle delete logic here
+    };
 
     return (
-        <div>
-            {
-                books.map((book) => (
-                    <div key={book.id}>
-                        <h3>{book.title}</h3>
-                        <p>{book.author}</p>
-                        <p>{book.publicationYear}</p>
-                        <p>{book.qty}</p>
-                        <p>{book.price}</p>
+        <div className="container">
+            <div className="row">
+                {books.map((book) => (
+                    <div className="col-md-6" key={book._id}>
+                        <div className="card mb-3" style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
+                            <div className="card-body">
+                                <h3 className="card-title">{book.title}</h3>
+                                <p className="card-text">Author: {book.author}</p>
+                                <p className="card-text">Publication Year: {book.publicationYear}</p>
+                                <p className="card-text">Quantity: {book.qty}</p>
+                                <p className="card-text">Price: {book.price}</p>
+                                <button className="btn btn-primary" onClick={() => handleUpdate(book.id)}>Update</button>
+                                <button className="btn btn-danger" onClick={() => handleDelete(book.id)}>Delete</button>
+                            </div>
+                        </div>
                     </div>
-                ))
-            }
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default AdminScreen;
