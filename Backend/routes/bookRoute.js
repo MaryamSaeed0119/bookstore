5d8a46e450bdf0013ee6128df68e625888b50bd1
MaryamSaeed0119 <144810857+MaryamSaeed0119@users.noreply.github.com>
2023-12-18 20:34:16 +0000

    Added search filters and buy routes

--- Backend/routes/bookRoute.js
+++ Backend/routes/bookRoute.js
@@ -72,4 +72,82 @@ router.delete('/books/:id', async (req, res) => {
   }
 });
 
+// Route to buy a book by ID
+router.post('/books/:id/buy', async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const updatedBook = await Book.findByIdAndUpdate(
+      bookId,
+      { $inc: { qty: -1 } }, // Decrement the quantity by 1
+      { new: true }
+    );
+
+    if (!updatedBook) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+
+    if (updatedBook.qty < 0) {
+      return res.status(400).json({ error: 'Book out of stock' });
+    }
+
+    res.json(updatedBook);
+  } catch (error) {
+    console.error('Error buying book by ID:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Route to get books by genre
+router.get('/books/genre/:genre', async (req, res) => {
+  try {
+    const genre = req.params.genre;
+    const books = await Book.find({ genre });
+
+    if (books.length === 0) {
+      return res.status(404).json({ error: `No books found in the genre "${genre}"` });
+    }
+
+    res.json(books);
+  } catch (error) {
+    console.error('Error getting books by genre:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Route to get books by title
+router.get('/books/title/:title', async (req, res) => {
+  try {
+    const title = req.params.title;
+    const books = await Book.find({ title });
+
+    if (books.length === 0) {
+      return res.status(404).json({ error: `No books found with the title "${title}"` });
+    }
+
+    res.json(books);
+  } catch (error) {
+    console.error('Error getting books by title:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// Route to get books by author
+router.get('/books/author/:author', async (req, res) => {
+  try {
+    const author = req.params.author;
+    const books = await Book.find({ author });
+
+    if (books.length === 0) {
+      return res.status(404).json({ error: `No books found by the author "${author}"` });
+    }
+
+    res.json(books);
+  } catch (error) {
+    console.error('Error getting books by author:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 module.exports = router;
