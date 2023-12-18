921b7f992a20772d12bc4c1c138b5b1b428871fc
MaryamSaeed0119 <144810857+MaryamSaeed0119@users.noreply.github.com>
2023-12-18 19:41:27 +0000

    Added CRUD of Books

--- Backend/routes/bookRoute.js
+++ Backend/routes/bookRoute.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models/bookModel');
 
+
+// Route to get a specific book by ID
+router.get('/books/:id', async (req, res) => {
+    
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.json(book);
+  } catch (error) {
+    console.error('Error getting book by ID:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 // Route to get all books
 router.get('/books', async (req, res) => {
   try {
@@ -13,6 +30,8 @@ router.get('/books', async (req, res) => {
   }
 });
 
+
+
 // Route to add a new book
 router.post('/books', async (req, res) => {
   try {
@@ -21,6 +40,34 @@ router.post('/books', async (req, res) => {
     res.json(newBook);
   } catch (error) {
     console.error('Error adding book:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Route to update a book by ID
+router.put('/books/:id', async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedBook) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.json(updatedBook);
+  } catch (error) {
+    console.error('Error updating book by ID:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Route to delete a book by ID
+router.delete('/books/:id', async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+    if (!deletedBook) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.json(deletedBook);
+  } catch (error) {
+    console.error('Error deleting book by ID:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
