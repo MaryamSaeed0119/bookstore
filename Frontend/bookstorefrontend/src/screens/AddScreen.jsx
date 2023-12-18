21046c96ca2d645dfc7a52e190e3e370507b0a9a
MaryamSaeed0119 <144810857+MaryamSaeed0119@users.noreply.github.com>
2023-12-18 21:56:27 +0000

    Imrpoved Add Screen UI

--- Frontend/bookstorefrontend/src/screens/AddScreen.jsx
+++ Frontend/bookstorefrontend/src/screens/AddScreen.jsx
@@ -1,60 +1,94 @@
 import React, { useState } from 'react';
+import { Form, Button, Card } from 'react-bootstrap';
 
 const AddScreen = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [genre, setGenre] = useState('');
-  const [publishYear, setPublishYear] = useState('');
-  const [price, setPrice] = useState('');
-  const [qty, setQty] = useState('');
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [genre, setGenre] = useState('');
+    const [publishYear, setPublishYear] = useState('');
+    const [price, setPrice] = useState('');
+    const [qty, setQty] = useState('');
 
-  const handleAddBook = () => {
-    // Logic to add the book with the entered details
-    // You can use the state variables (title, author, genre, publishYear, price, qty) to access the entered values
-  };
+    const handleAddBook = () => {
+        // Logic to add the book with the entered details
+        // You can use the state variables (title, author, genre, publishYear, price, qty) to access the entered values
+    };
 
-  return (
-    <div>
-      <h1>Add Book</h1>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Author"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Genre"
-        value={genre}
-        onChange={(e) => setGenre(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Publish Year"
-        value={publishYear}
-        onChange={(e) => setPublishYear(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Price"
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Quantity"
-        value={qty}
-        onChange={(e) => setQty(e.target.value)}
-      />
-      <button onClick={handleAddBook}>Add Book</button>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Add Book</h1>
+            <Card style={{ width: '35rem', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)' }}>
+                <Card.Body>
+                    <Form>
+                        <Form.Group controlId="formTitle">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter title"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formAuthor">
+                            <Form.Label>Author</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter author"
+                                value={author}
+                                onChange={(e) => setAuthor(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formGenre">
+                            <Form.Label>Genre</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter genre"
+                                value={genre}
+                                onChange={(e) => setGenre(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formPublishYear">
+                            <Form.Label>Publish Year</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter publish year"
+                                value={publishYear}
+                                onChange={(e) => setPublishYear(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formPrice">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter price"
+                                value={price}
+                                onChange={(e) => setPrice(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="formQty">
+                            <Form.Label>Quantity</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter quantity"
+                                value={qty}
+                                onChange={(e) => setQty(e.target.value)}
+                            />
+                        </Form.Group>
+                        <br>
+                        </br>
+                        <Button variant="primary" onClick={handleAddBook}>
+                            Add Book
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 };
 
 export default AddScreen;
